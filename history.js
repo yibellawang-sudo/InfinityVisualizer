5a1b2c0986ccc877c9ffc5558ec93fd8d5a78556
yibellawang-sudo <yi.bella.wang@gmail.com>
2025-10-29 10:03:49 -0400

    updated intro page format

--- history.js
+++ history.js
@@ -1,17 +1,1 @@
-document.querySelectorAll('nav a').forEach(link => {
-    link.addEventListener('click', (e) => {
-        e.preventDefault();
-        const targetId = link.getAttribute('href').substring(1);
-        const targetSection = document.getElementById(targetId);
 
-        if (targetSection) {
-            targetSection.scrollIntroView({
-                behavior: 'smooth',
-                block: 'start'
-            });
-        }
-    });
-});
-
-//Make video showing off project
-//write project description
